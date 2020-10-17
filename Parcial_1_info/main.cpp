16667524b4b567068b264313ada30f62c470289a
Daniel <daniel.perez19@udea.edu.co>
2020-10-17 17:30:46 -0500

    Quinto y ultimo commit (El tiempo no rindio :c)

--- Parcial_1_info/main.cpp
+++ Parcial_1_info/main.cpp
@@ -11,16 +11,15 @@ float times (float,int);
 
 int main()
 {
-    int hd=0,ho=0,v0d=0,v0o=0, angd=0,ango=0;
-    float d=0, dx=0,dy=0, timed=0, timeo=0;
-    hd= random(1,0,100);
+    int hd=0,ho=0,v0d=0,v0o=0, angd=0,ango=0,x=0,y=0;
+    float d=0, dx=0,dy=0, timed=0, timeo=0,eje_x_o=0;
+    hd= random(1,1,100);
     cout << "Altura canon defensivo: " << hd << endl;
-    ho= random(1,0,100);
+    ho= random(1,1,100);
     cout << "Altura canon ofensivo: " << hd << endl;
-    int posd[2]= {100, hd},poso[2]= {1, ho};//Posiciones de los dos cañones
+    int posd[2]= {300, hd},poso[2]= {1, ho};//Posicion del canon ofensivo
 
-    v0d=random(1,0,100);//Velocidades random del cañon inicial
-    cout << "velocidad canon defensivo: " << v0d << endl;
+
     v0o=random(1,0,100);//Velocidades random del cañon inicial
     cout << "velocidad canon ofensivo: " << v0o << endl;
 
@@ -30,15 +29,28 @@ int main()
     dy=dy*dy;
     d=dx+dy;
     d= sqrt(d);//Pitagorazo para saber la distancia con los valores de los cañones
-    cout << endl << d;
+    cout <<"Distancia entre los canones: " << d<< endl;
 
-    timed= times(d,v0d);//tiempo difensivo
     timeo= times(d,v0o);//tiempo ofensivo
 
-    angd=random(1,0,89);//angulo random del cañon inicial
-    cout << "angulo canon defensivo: " << angd << endl;
-    ango=random(1,1,89);//angulo random del cañon inicial
+    ango=random(1,91,180);//angulo random del cañon inicial
     cout << "angulo canon ofensivo: " << ango << endl;
+
+    //Eje horizontal x del ofensivo
+    eje_x_o=(v0o*timeo)+poso[1];
+    cout << endl << eje_x_o;
+
+    //alcance maximo cañon ofensivo
+    x=(v0o*v0o)*cos(ango)/9.81;
+    cout << "Alcance en X maximo: " << x;
+
+    if (x==poso[0]){
+       //El cañón ofensivo es peligroso y va a golpear al cañon defensivo, requiere defenderse
+
+
+}
+
+
 
 
 }
