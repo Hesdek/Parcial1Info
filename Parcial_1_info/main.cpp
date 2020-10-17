a38b9a524efecd5141f6816ab727b8f444d12369
Daniel <daniel.perez19@udea.edu.co>
2020-10-17 15:44:29 -0500

    Primer commit del codigo, funcion random

--- Parcial_1_info/main.cpp
+++ Parcial_1_info/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+
+int random (int);
+
 int main()
 {
     int n, cont=0;
@@ -10,4 +13,11 @@ int main()
     //for (int i=0; i<100; i++)
 
 
+}
+int random (int n){
+    int aleatorio, desde=0, hasta=100;
+    for (int i = 1; i <= n; i ++){
+    aleatorio = rand()%(hasta-desde+1)+desde;
+    return aleatorio;
+            }
 }
