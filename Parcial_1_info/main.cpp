ff35e3bd66d30ba70167a693c04e3cc5eb01e037
Daniel <daniel.perez19@udea.edu.co>
2020-10-17 15:58:22 -0500

    Segundo commit del codigo, se usan las fucniones

--- Parcial_1_info/main.cpp
+++ Parcial_1_info/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
+#include<time.h>
+#include<math.h>
 using namespace std;
 
 
-int random (int);
+int random (int,int,int);
 
 int main()
 {
-    int n, cont=0;
-    cout << "Ingrese la cadena 1: ";
-    cin>>n;
+    int n, cont=0,hd=0,ho=0;
+    hd= random(1,0,100);
+    cout << "Altura canon defensivo: " << hd << endl;
+    ho= random(1,0,100);
+    cout << "Altura canon ofensivo: " << hd << endl;
+
+
+
+
     //while(array[cont]!='\0'){cont++;}
     //for (int i=0; i<100; i++)
 
 
 }
-int random (int n){
-    int aleatorio, desde=0, hasta=100;
+int random (int n, int desde, int hasta){
+    int aleatorio;
     for (int i = 1; i <= n; i ++){
     aleatorio = rand()%(hasta-desde+1)+desde;
     return aleatorio;
